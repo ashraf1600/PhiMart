c24b890a833d82459d4284c6c4a73b172bde7d9a
Ashraful Islam <ashrafdcc1502@gmail.com>
2026-05-11 15:38:44 +0600

    migrated

--- order/admin.py
+++ order/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from order.models import Cart , CartItem
+from order.models import Cart , CartItem, Order, OrderItem
 
 # Register your models here.
 @admin.register(Cart)
@@ -7,4 +7,9 @@ class CartAdmin(admin.ModelAdmin):
     list_display = ['id','user']
 
 
+@admin.register(Order)    
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['id','user','status','total_price']
+
 admin.site.register(CartItem)
+admin.site.register(OrderItem)
