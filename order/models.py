ca8dd5ee0ed78d92afd1acd38c3af7f1d3c4431d
Ashraful Islam <ashrafdcc1502@gmail.com>
2026-05-11 15:01:34 +0600

    Order model updated and migration done

--- order/models.py
+++ order/models.py
@@ -37,21 +37,23 @@ class Order(models.Model):
     READY_TO_SHIP = 'Ready to Ship'
     SHIPPED = 'Shipped'
     DELIVERD = 'Delivered'
+    CANCELED = 'Canceled'
     STATUS_CHOICES = [
         (NOT_PAID, 'Not Paid'),
         (READY_TO_SHIP, 'Ready to Ship'),
         (SHIPPED, 'Shipped'),
         (DELIVERD, 'Delivered'),
+        (CANCELED, 'Canceled'),
     ]
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='orders')
-    status = models.CharField(max_length=10, choices=STATUS_CHOICES, default=PENDING)
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default=NOT_PAID)
     total_price = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
    
 
     def __str__(self):
-        return f"Order {self.id} by {self.user.username} on {self.created_at}"
+        return f"Order {self.id} by {self.user.username} on {self.created_at} with status {self.status}"
     
 
 class OrderItem(models.Model):
