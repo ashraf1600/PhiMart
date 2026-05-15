edf79b4e6b9af9247939a3877ae5d2fa34cf1c6e
Ashraful Islam <ashrafdcc1502@gmail.com>
2026-05-15 21:44:11 +0600

    ProductImage model created and migrated

--- product/models.py
+++ product/models.py
@@ -24,6 +24,15 @@ class Product(models.Model):
 
     def __str__(self):
         return self.name
+    
+
+class ProductImage(models.Model):
+    product = models.ForeignKey(Product, related_name='images', on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='products/')
+    alt_text = models.CharField(max_length=255, blank=True, null=True)
+
+    def __str__(self):
+        return f"Image for {self.product.name}"    
 
 class Review(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='reviews')
